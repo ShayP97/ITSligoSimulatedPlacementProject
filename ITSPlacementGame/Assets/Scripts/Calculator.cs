48e8718e0c1320fae87fcef5004558ccc0be106f
ShayP97 <goldenedge37@gmail.com>
2018-04-13 19:37:03 +0100

    Calculator partially working

---
+++ ITSPlacementGame/Assets/Scripts/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Calculator : MonoBehaviour
+{
+    [SerializeField]
+    public Text InputField;
+
+    string inputValue;
+
+    int[] Num = new int[2];
+
+    int argument;
+    int i = 0;
+
+    public void ButtonSelected()
+    {
+        string bValue = EventSystem.current.currentSelectedGameObject.name;
+        InputField.text = bValue;
+
+        if(int.TryParse(bValue, out argument))
+        {
+            if (i > 1) i = 0;
+            Num[i] = argument;
+            i = i++;   
+        }
+
+        inputValue += bValue;
+        InputField.text = inputValue;
+        Num = new int[2];
+
+    }	
+
+}
