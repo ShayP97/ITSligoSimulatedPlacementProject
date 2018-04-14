6ae4406b3ed2acff466b7d6f000f7164eeefe869
ShayP97 <goldenedge37@gmail.com>
2018-04-14 19:10:25 +0100

    Calculator fixed and adjusted

--- ITSPlacementGame/Assets/Scripts/Calculator.cs
+++ ITSPlacementGame/Assets/Scripts/Calculator.cs
@@ -8,31 +8,41 @@ using UnityEngine.UI;
 public class Calculator : MonoBehaviour
 {
     [SerializeField]
-    public Text InputField;
+    public Text inputField;
 
     string inputValue;
 
-    int[] Num = new int[2];
+    string enter;
 
-    int argument;
-    int i = 0;
+    //int[] Num = new int[2];
+
+    //int argument;
+    //int i = 0;
 
     public void ButtonSelected()
     {
-        string bValue = EventSystem.current.currentSelectedGameObject.name;
-        InputField.text = bValue;
+        string bValue = EventSystem.current.currentSelectedGameObject.name; //grabs the name of the button that was pressed
 
-        if(int.TryParse(bValue, out argument))
-        {
-            if (i > 1) i = 0;
-            Num[i] = argument;
-            i = i++;   
-        }
+        inputField.text += bValue; //displays the numbers that were selected
+        enter = inputField.text;
 
-        inputValue += bValue;
-        InputField.text = inputValue;
-        Num = new int[2];
+
+        //if(int.TryParse(bValue, out argument))
+        //{
+        //    if (i > 1) i = 0;
+        //    Num[i] = argument;
+        //    i = i++;   
+        //}
+
+        //inputValue += bValue;
+
+        //inputField.text = inputValue;
 
     }	
+
+    public void BackButton()
+    {
+        inputField.text = " "; //Clears the current display
+    }
 
 }
