84e3966ab843669c7b8b64fa71db69faee35c1c4
ShayP97 <goldenedge37@gmail.com>
2018-04-16 15:15:17 +0100

    Calculator now displays and stores correct answers

--- ITSPlacementGame/Assets/Scripts/Calculator.cs
+++ ITSPlacementGame/Assets/Scripts/Calculator.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ *Authour: Shay Pitcher
+ * 
+ * Function: Allows the user to input their answer to the current question
+*/
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,39 +16,47 @@ public class Calculator : MonoBehaviour
     [SerializeField]
     public Text inputField;
 
+    public GameObject correctPanel;
+
+    public GameObject wrongPanel;
+
     string inputValue;
 
     string enter;
 
-    //int[] Num = new int[2];
-
-    //int argument;
-    //int i = 0;
+    string answer;
 
     public void ButtonSelected()
     {
         string bValue = EventSystem.current.currentSelectedGameObject.name; //grabs the name of the button that was pressed
 
         inputField.text += bValue; //displays the numbers that were selected
-        enter = inputField.text;
-
-
-        //if(int.TryParse(bValue, out argument))
-        //{
-        //    if (i > 1) i = 0;
-        //    Num[i] = argument;
-        //    i = i++;   
-        //}
-
-        //inputValue += bValue;
-
-        //inputField.text = inputValue;
-
     }	
 
     public void BackButton()
     {
         inputField.text = " "; //Clears the current display
+
+        if (wrongPanel == true)
+        {
+            wrongPanel.SetActive(false);
+        }
+
+    }
+
+    public void OKButton()
+    {
+        answer = inputField.text; //graps the current answer and stores it
+
+        if(answer == "3.75") //checks for the correct answer
+        {
+            correctPanel.SetActive(true); //turns on the panel to display "Correct!"            
+        }
+        else
+        {
+            wrongPanel.SetActive(true);
+        }
+
     }
 
 }
