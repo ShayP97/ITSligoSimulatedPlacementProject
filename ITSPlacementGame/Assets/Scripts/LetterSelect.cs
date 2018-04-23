756194db6639212489009596acb9af7d5f0d47a1
ShayP97 <goldenedge37@gmail.com>
2018-04-23 16:53:09 +0100

    Added text input into the Book Store minigame

---
+++ ITSPlacementGame/Assets/Scripts/LetterSelect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class LetterSelect : MonoBehaviour
+{
+    [SerializeField]
+    public Text inputBox1;
+    public Text inputBox2;
+
+    string letter;
+
+    public void SelectLetter()
+    {
+        letter = EventSystem.current.currentSelectedGameObject.name; //grabs the name of the button that was pressed
+
+        inputBox1.text = letter; //displays the numbers that were selected
+        inputBox2.text = letter;
+    }
+
+}
