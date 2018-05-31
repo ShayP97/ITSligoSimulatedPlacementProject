8c44b525b05d2b41128e27741a39d9c0f390b5f6
ShayP97 <goldenedge37@gmail.com>
2018-05-31 17:03:04 -0400

    Settings panel added and mute button moved

---
+++ ITSPlacementGame/Assets/Scripts/Settings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    public GameObject settingsPanel;
+
+    public void openPanel()
+    {
+        settingsPanel.SetActive(true);
+        Time.timeScale = 0.0f;
+    }
+    public void ClosePanel()
+    {
+        settingsPanel.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+}
