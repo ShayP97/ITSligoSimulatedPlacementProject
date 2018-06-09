b6607790b4fd3bf68112363484d6fb97d8dd92b2
ShayP97 <goldenedge37@gmail.com>
2018-06-09 13:30:55 -0400

    Music now plays continuolsy through the Menu scenes (Script created)

---
+++ ITSPlacementGame/Assets/Scripts/MusicPlaying.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlaying : MonoBehaviour
+{
+    private static MusicPlaying instance = null;
+    public static MusicPlaying Instance
+    {
+        get{ return instance; }
+    }
+
+    private void Awake()
+    {
+        //Keeps the Menu music playing through all of the menu scenes
+        if(instance != null && instance != this )
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        //Stops the Menu music from playing after it enters the Main Scene
+        if (SceneManager.GetActiveScene().name == "Main Scene") 
+        {
+            Destroy(gameObject);
+        }
+    }
+}
