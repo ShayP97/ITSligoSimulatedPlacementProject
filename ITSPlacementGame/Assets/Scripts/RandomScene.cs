21a71309fa714c38a5990f7aceacd925a1bc1a47
ShayP97 <goldenedge37@gmail.com>
2018-07-10 21:34:22 -0400

    More testing done and small fixes made

---
+++ ITSPlacementGame/Assets/Scripts/RandomScene.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RandomScene : MonoBehaviour
+{
+    string Cafe1;
+    string Cafe2;
+    string Cafe3;
+
+    private int randomScene;
+
+    private void Start()
+    {
+        randomScene = Random.Range(0, 4);
+        
+         if(randomScene == 1)
+        {
+            Debug.Log("Scene1");
+            SceneManager.LoadScene("Cafe");
+        }
+
+        if (randomScene == 2)
+        {
+            Debug.Log("Scene2");
+            SceneManager.LoadScene("CafeMinigame1Q2");
+        }
+
+        if (randomScene == 3)
+        {
+            Debug.Log("Scene3");
+            SceneManager.LoadScene("CafeMinigame1Q3");
+        }
+    }
+
+}
