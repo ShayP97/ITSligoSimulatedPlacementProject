7ec84174f987c0ef214f4a73a15f6c440914002b
ShayP97 <goldenedge37@gmail.com>
2018-07-14 18:00:02 -0400

    Added fad in text to the Cafe and Post office outro scenes and changed the game font

---
+++ ITSPlacementGame/Assets/Scripts/FadeText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeText : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        gameObject.GetComponent<Graphic>().FadeText();
+	}
+
+}
---
+++ ITSPlacementGame/Assets/Scripts/TextFadeIn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class TextFadeIn
+{
+    public static void FadeText(this Graphic g)
+    {
+        g.GetComponent<CanvasRenderer>().SetAlpha(0f);
+        g.CrossFadeAlpha(1f, 2f, false);
+    }
+
+
+}
